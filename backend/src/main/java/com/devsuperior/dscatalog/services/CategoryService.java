2972fd17220f6ab2ede9d72d58bca71c4f01cffc
Adiel Dantas <adieldantasn@gmail.com>
2025-05-20 15:11:20 -0300

    Category ORM, Category CRUD, H2 database

---
+++ backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -0,0 +1,74 @@
+package com.devsuperior.dscatalog.services;
+
+import com.devsuperior.dscatalog.dto.CategoryDTO;
+import com.devsuperior.dscatalog.entities.Category;
+import com.devsuperior.dscatalog.repositores.CategoryRepository;
+import com.devsuperior.dscatalog.services.exceptions.DataBaseException;
+import com.devsuperior.dscatalog.services.exceptions.ResourcerNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class CategoryService {
+
+    @Autowired
+    private CategoryRepository repository;
+
+    @Transactional(readOnly = true)
+    public Page<CategoryDTO> findAllPaged (Pageable pageable) {
+        Page<Category> list = repository.findAll(pageable);
+        return list.map(x -> new CategoryDTO(x));
+    }
+
+    @Transactional(readOnly = true)
+    public CategoryDTO findById(Long id) {
+        Category category = repository.findById(id).orElseThrow(
+                () -> new ResourcerNotFoundException("Recurso não localizada"));
+        return new CategoryDTO(category);
+    }
+
+    @Transactional
+    public CategoryDTO insert (CategoryDTO dto) {
+        Category category = new Category();
+        copyDtoToEntity(dto, category);
+        category = repository.save(category);
+        return new CategoryDTO(category);
+    }
+
+    @Transactional
+    public CategoryDTO update(Long id, CategoryDTO dto) {
+        try {
+            Category category = repository.getReferenceById(id);
+            copyDtoToEntity(dto, category);
+            category = repository.save(category);
+            return new CategoryDTO(category);
+        }
+        catch (EntityNotFoundException e) {
+            throw new ResourcerNotFoundException("Id não localizado: " + id);
+        }
+    }
+
+    @Transactional(propagation = Propagation.SUPPORTS)
+    public void delete(Long id) {
+        if (!repository.existsById(id)) {
+            throw new ResourcerNotFoundException("Id não localizado: " + id);
+        }
+        try {
+            repository.deleteById(id);
+        }
+        catch (DataIntegrityViolationException e) {
+            throw new DataBaseException("Violação de integridade");
+        }
+    }
+
+    private void copyDtoToEntity(CategoryDTO dto, Category category) {
+        category.setName(dto.getName());
+    }
+
+}
---
+++ backend/src/main/java/com/devsuperior/dscatalog/services/exceptions/ResourcerNotFoundException.java
@@ -0,0 +1,8 @@
+package com.devsuperior.dscatalog.services.exceptions;
+
+public class ResourcerNotFoundException extends RuntimeException{
+
+    public ResourcerNotFoundException(String msg) {
+        super(msg);
+    }
+}
